61eeba9cc61a378118f8b909a79202ffeed70b77
Carsten Klaffke <mail@carsten-klaffke.de>
2026-01-22 11:28:46 +0100

    Capacitor 8 Product name correction

--- Package.swift
+++ Package.swift
@@ -2,27 +2,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "SendIntent",
+    name: "MindlibCapacitorSendIntent",
     platforms: [.iOS(.v15)],
     products: [
         .library(
-            name: "SendIntent",
-            targets: ["SendIntentPlugin"])
+            name: "MindlibCapacitorSendIntent",
+            targets: ["MindlibCapacitorSendIntent"])
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
     ],
     targets: [
         .target(
-            name: "SendIntentPlugin",
+            name: "MindlibCapacitorSendIntent",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
                 .product(name: "Cordova", package: "capacitor-swift-pm")
             ],
             path: "ios/Sources/SendIntentPlugin"),
         .testTarget(
-            name: "SendIntentPluginTests",
-            dependencies: ["SendIntentPlugin"],
+            name: "MindlibCapacitorSendIntentTests",
+            dependencies: ["MindlibCapacitorSendIntent"],
             path: "ios/Tests/SendIntentPluginTests")
     ]
 )
